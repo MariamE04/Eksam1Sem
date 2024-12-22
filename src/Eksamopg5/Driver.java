1b1a845c63e4a4832c693bb4c8efd9d9e86629ce
Mariam <elmirmariam16@gmail.com>
2024-12-22 16:54:44 +0100

    laver klassene

---
+++ src/Eksamopg5/Driver.java
@@ -0,0 +1,13 @@
+package Eksamopg5;
+
+public class Driver {
+    public String name;
+    public boolean isbooked;
+    public int [] districtCoverage;
+
+    public Driver(String name, boolean isbooked, int[] districtCoverage) {
+        this.name = name;
+        this.isbooked = isbooked;
+        this.districtCoverage = districtCoverage;
+    }
+}
---
+++ src/Eksamopg5/RickshawService.java
@@ -0,0 +1,23 @@
+package Eksamopg5;
+
+import java.util.ArrayList;
+
+public class RickshawService {
+    private String [][] districts;
+    private ArrayList<Driver> drivers;
+
+
+    public Driver creatDriver(){
+        return null;
+    }
+
+    public void bookDriver(){
+
+    }
+
+    private String findDriver(int districtNumber){
+        return null;
+    }
+
+
+}
