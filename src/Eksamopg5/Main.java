6b282b4c3b047d34c1098bf926909220a203df91
Mariam <elmirmariam16@gmail.com>
2024-12-31 15:42:53 +0100

    færdig

--- src/Eksamopg5/Main.java
+++ src/Eksamopg5/Main.java
@@ -8,7 +8,7 @@ public class Main {
         // Opretter nogle chauffører og deres dækningsområder
         int[] driver1Coverage = {1}; // Driver 1 dækker distrikt 1 og 2
         int[] driver2Coverage = {2}; // Driver 2 dækker distrikt 3
-        int[] driver3Coverage = {1, 3}; // Driver 3 dækker distrikt 1 og 3
+        int[] driver3Coverage = {3}; // Driver 3 dækker distrikt 1 og 3
 
         service.creatDriver("Anders", driver1Coverage);
         service.creatDriver("Maria", driver2Coverage);
