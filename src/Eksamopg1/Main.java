109fbde882f10c31e040f2294e610df0cca6013c
Mariam <elmirmariam16@gmail.com>
2025-01-02 13:57:47 +0100

    færdig

--- src/Eksamopg1/Main.java
+++ src/Eksamopg1/Main.java
@@ -5,8 +5,14 @@ public class Main {
        Basket basket = new Basket();
 
        basket.addProduct(new Clothes("T-shirt", 49.85, "black", 10));
+       basket.addProduct(new Clothes("Sweater",52.01, "White", 8));
+
+
        basket.addProduct(new Electronic("Headset", 80.00,8) );
+
+
        basket.addProduct(new Food("Milk", 1.68, "21. December 2024"));
+
 
        basket.printAll();
     }
